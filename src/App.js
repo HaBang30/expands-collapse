2f0cd86c67eeec4e339ea2ab02ea1fb76935a731
HaBang30 <doanduytung1602@gmail.com>
2024-01-10 15:07:54 +0700

    add the way 3

--- src/App.js
+++ src/App.js
@@ -36,40 +36,88 @@
 
 // export default App;
 
-import "./App.css"
+
+// ==== The way 2 ====
+// import "./App.css"
+
+// import React, {Component} from "react";
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       login: true
+//     }
+//   }
+//   handleLogIn =() => {
+//     let content = document.getElementById("Content");
+//     if(this.state.login) {
+//       content.style.display = "block"
+//     }else {
+//       content.style.display = "none"
+//     }
+//     this.setState({
+//       login:!this.state.login
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//           <h1>Conditional Rendering</h1>
+//           <button onClick={this.handleLogIn}>{this.state.login? "Show Introduction" : "Close Introduction"}</button>
+//           <p id="Content"> In ReactJs, sometimes you have e few  component and depends on  conditions, Example as state, props,... <br/>
+//               You want to show one or more component. The  you can to use conditional rendering to render component you want 
+//           </p>
+//       </div>
+      
+
+//     )
+//   }
+// }
+// export default App;
+
+
+// ==== The Way 3 ======
+
+import "./App.css";
 
 import React, {Component} from "react";
 
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
-      login: true
+      isExpand: false
     }
   }
-  handleLogIn =() => {
-    let content = document.getElementById("Content");
-    if(this.state.login) {
-      content.style.display = "block"
-    }else {
-      content.style.display = "none"
-    }
-    this.setState({
-      login:!this.state.login
-    })
+  showIntroduction =() => {
+    this.setState({isExpand:!this.state.isExpand})
   }
+
   render() {
+    
+    if(this.state.isExpand) {
+      return (<div>
+              <h1>Conditional Rendering</h1>
+              <button onClick={this.showIntroduction}>Close Introduction</button>
+              <h2>Introduction</h2>
+              <p> In ReactJs, depend on component condition like state and props....<br/>
+                  you want to display one or more components, so you can use conditional rendering to render output component you want
+              </p>
+            </div>)
+        
+
+    } 
     return (
-      <div>
-          <h1>Conditional Rendering</h1>
-          <button onClick={this.handleLogIn}>{this.state.login? "Show Introduction" : "Close Introduction"}</button>
-          <p id="Content"> In ReactJs, sometimes you have e few  component and depends on  conditions, Example as state, props,... <br/>
-              You want to show one or more component. The  you can to use conditional rendering to render component you want 
-          </p>
+      <div className="container">
+        <h1>Conditional Rendering</h1>
+        <button onClick={this.showIntroduction}>Show Introduction</button>
+        
       </div>
-      
 
     )
   }
 }
+
 export default App;
+//====== The End =====
